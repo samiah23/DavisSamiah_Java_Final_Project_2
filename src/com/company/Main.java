57cb6de64f2b114aafbc78caabbc6e5d71102263
Samiah Davis <samiah@wireless-10-105-43-38.umd.edu>
2022-04-13 15:55:27 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,81 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> products = new ArrayList<>();
+        products.add("Red-Hot Spicy Doritos");
+        products.add("Cool Ranch Doritos");
+        products.add("Coke");
+        products.add("Pepsi");
+        products.add("Diet Coke");
+        products.add("Five Hour Energy");
+        products.add("Mac Charger");
+        products.add("Dell Charger");
+        products.add("Sunflower Seeds");
+        products.add("Peanuts");
+
+        ArrayList<Double> productsprice = new ArrayList<>();
+        productsprice.add(2.99);
+        productsprice.add(2.99);
+        productsprice.add(0.99);
+        productsprice.add(0.99);
+        productsprice.add(0.99);
+        productsprice.add(3.99);
+        productsprice.add(120.0);
+        productsprice.add(50.0);
+        productsprice.add(0.99);
+        productsprice.add(0.99);
+
+        ArrayList<String> CustomerItem = new ArrayList<>();
+        ArrayList<Integer> CustomerItemAmounts = new ArrayList<>();
+        String input;
+        Scanner stringInput = new Scanner(System.in);
+
+        StringBuilder output = new StringBuilder();
+        double total;
+        double grandTotal = 0.00;
+        System.out.print("\nEnter the name of the customer: ");
+        while(!(input = stringInput.nextLine()).equalsIgnoreCase("0"))   {
+            output.append("\n").append(input);
+            total = 0.00;
+            System.out.println("Enter the name of items: (0 to end");
+
+
+            for(int i = 0; i < products.size(); i++) {
+                System.out.println(products.get(i) + " -------> " +  productsprice.get(i));
+            }
+
+
+            while(!(input = stringInput.nextLine()).equalsIgnoreCase("0")) {
+                for(String i:products) {
+                    if(i.contains(input) || i.equalsIgnoreCase(input)) {
+                        total += productsprice.get(products.indexOf(i));
+                        output.append("\n\t").append(input);
+                        if(!CustomerItem.contains(input)) {
+                            CustomerItem.add(input);
+                            CustomerItemAmounts.add(1);}
+                        else {
+                            int index = CustomerItem.indexOf(input);
+                            CustomerItemAmounts.set(index, CustomerItemAmounts.get(index) + 1); }
+                        break; }
+                }
+            }
+            output.append("\n\t\t").append(total).append("\n");
+            grandTotal += total;
+            System.out.print("\nEnter the name of the next customer (0 to end): ");
+        }
+        System.out.println("Receipt: \n"+output);
+        System.out.println("\nProducts Sold: ");
+        for(String i:CustomerItem) {
+            System.out.println("\t"+i+" ("+CustomerItemAmounts.get(CustomerItem.indexOf(i))+")");
+        }
+        System.out.println("\nGrand Total: "+grandTotal);
+    }
+}
+
+
